71ce3f38cbc7b6651ddfe47791362c49baa46b46
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-07-18 15:15:59 -0400

    prettier

--- src/server.js
+++ src/server.js
@@ -5,21 +5,20 @@ const app = express()
 const port = 3000
 
 const onUpload = (request, response, next) => {
-  console.log(request.headers['content-type'])
-  getRawBody(request, {
-    length: request.headers['content-length'],
-    limit: '5gb'
-  })
-    .then(zipData => {
-      console.log(`parsed raw body.  ${zipData.length}`)
-      response.send(`Hello World! Size is ${zipData.length}\n`)
+    console.log(request.headers['content-type'])
+    getRawBody(request, {
+        length: request.headers['content-length'],
+        limit: '5gb'
     })
-    .catch(err => {
-      console.err(err)
-      next(err)
-    })
+        .then(zipData => {
+            console.log(`parsed raw body.  ${zipData.length}`)
+            response.send(`Hello World! Size is ${zipData.length}\n`)
+        })
+        .catch(err => {
+            console.err(err)
+            next(err)
+        })
 }
-
 
 app.use(express.static('frontend/public'))
 app.post('/upload', onUpload)
