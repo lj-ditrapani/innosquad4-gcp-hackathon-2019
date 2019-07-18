8c4317b77cd891be93a357232fb76a2be39a70e6
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-07-18 10:48:40 -0400

    Serve static files

--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('frontend/build'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
