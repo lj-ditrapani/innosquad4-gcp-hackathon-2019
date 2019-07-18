6598b925e21f5a835bb80f152fbdd0b9af916033
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-07-18 10:58:10 -0400

    frontend

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.use(express.static('frontend/build'))
+app.post('/upload', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
