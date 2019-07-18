afd56f02ad0bb97555c098b32df3891ddc6d9607
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-07-18 16:53:24 -0400

    File input

--- frontend/public/assets/js/main.js
+++ frontend/public/assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function hello(files) {
+    console.log("I get files")
+}
+
 ;(function($) {
     var $window = $(window),
         $body = $('body'),
