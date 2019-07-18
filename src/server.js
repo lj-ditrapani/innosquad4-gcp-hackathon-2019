8e0f8dbb52033d6fe9296956f722772a0f758b2d
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-07-18 14:32:10 -0400

    Get binary post data

--- src/server.js
+++ src/server.js
@@ -1,8 +1,27 @@
 const express = require('express')
+var contentType = require('content-type')
+const getRawBody = require('raw-body')
 const app = express()
 const port = 3000
 
+const onUpload = (request, response) => {
+  const zipData = request.text
+  response.send(`Hello World! Size is ${zipData.length}`)
+}
+
+app.use(function (req, res, next) {
+  getRawBody(req, {
+    length: req.headers['content-length'],
+    limit: '5gb',
+    encoding: contentType.parse(req).parameters.charset
+  }, function (err, string) {
+    if (err) return next(err)
+    req.text = string
+    next()
+  })
+})
+
 app.use(express.static('frontend/public'))
-app.post('/upload', (req, res) => res.send('Hello World!'))
+app.post('/upload', onUpload)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
