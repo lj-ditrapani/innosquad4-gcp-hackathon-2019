b0e5e3144cefbec8a2438a38b2652950cb556fa9
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2019-07-18 16:47:45 -0400

    prettier

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const contentType = require('content-type')
 const getRawBody = require('raw-body')
-const JSZip = require("jszip")
+const JSZip = require('jszip')
 const app = express()
 const port = 3000
 
@@ -13,25 +13,27 @@ const onUpload = (request, response, next) => {
     })
         .then(rawZipBytes => {
             console.log(`parsed raw body.  ${rawZipBytes.length}`)
-            JSZip.loadAsync(rawZipBytes).then(zip =>
-                zip
-                .file("search_history/your_search_history.json")
-                .async("string")
-                .then(text => [zip, text])
-            ).then(pair => {
-                const [zip, text] = pair
-                console.log(text);
-                const json = JSON.parse(text)
-                console.log(json)
-                response.json({
-                    greeting: "hello world",
-                    size: rawZipBytes.length,
-                    lastSearch: json.searches[0].title,
-                    messages: getMessageData(zip),
-                    searchHistory: getSearchHistory(zip),
-                    locations: getLocationData(zip)
+            JSZip.loadAsync(rawZipBytes)
+                .then(zip =>
+                    zip
+                        .file('search_history/your_search_history.json')
+                        .async('string')
+                        .then(text => [zip, text])
+                )
+                .then(pair => {
+                    const [zip, text] = pair
+                    console.log(text)
+                    const json = JSON.parse(text)
+                    console.log(json)
+                    response.json({
+                        greeting: 'hello world',
+                        size: rawZipBytes.length,
+                        lastSearch: json.searches[0].title,
+                        messages: getMessageData(zip),
+                        searchHistory: getSearchHistory(zip),
+                        locations: getLocationData(zip)
+                    })
                 })
-            })
         })
         .catch(err => {
             console.err(err)
@@ -40,15 +42,15 @@ const onUpload = (request, response, next) => {
 }
 
 const getMessageData = zip => {
-    return { m1: "hi", m2: "by" }
+    return { m1: 'hi', m2: 'by' }
 }
 
 const getSearchHistory = zip => {
-    return { m1: "hi", m2: "by" }
+    return { m1: 'hi', m2: 'by' }
 }
 
 const getLocationData = zip => {
-    return { m1: "hi", m2: "by" }
+    return { m1: 'hi', m2: 'by' }
 }
 
 app.use(express.static('frontend/public'))
